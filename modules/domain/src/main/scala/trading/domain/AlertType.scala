bdc49d8e035f0a979578a36039097e1601f9664a
Gabriel Volpe <volpegabriel@gmail.com>
2021-10-04 18:08:08 +0200

    enum codec for alert type

--- modules/domain/src/main/scala/trading/domain/AlertType.scala
+++ modules/domain/src/main/scala/trading/domain/AlertType.scala
@@ -1,6 +1,14 @@
 package trading.domain
 
-import io.circe.Codec
+import cats.syntax.all.*
+import io.circe.{ Decoder, Encoder, Json }
 
-enum AlertType derives Codec.AsObject:
+enum AlertType:
   case StrongBuy, StrongSell, Neutral, Buy, Sell
+
+object AlertType:
+  given Decoder[AlertType] = Decoder[String].emap[AlertType] { str =>
+    Either.catchNonFatal(AlertType.valueOf(str)).leftMap(_.getMessage)
+  }
+
+  given Encoder[AlertType] = Encoder[String].contramap[AlertType](_.toString)
