16a7186513c919c77bd320b86fad7ee1077d82d5
Gabriel Volpe <volpegabriel@gmail.com>
2021-10-17 17:28:53 +0200

    add effectful context example

---
+++ modules/x-demo/src/main/scala/demo/EffectfulContext.scala
@@ -0,0 +1,61 @@
+package demo
+
+import java.util.UUID
+
+import scala.concurrent.duration.*
+
+import cats.effect.*
+import cats.effect.std.{ Console, Supervisor }
+import cats.syntax.all.*
+
+// https://docs.scala-lang.org/scala3/reference/contextual/context-functions.html
+object EffectfulContext extends IOApp.Simple:
+
+  final class Log(ref: Ref[IO, List[String]]):
+    def add(str: => String): IO[Unit] = ref.update(_ :+ str)
+    def get: IO[List[String]]         = ref.get
+
+  final case class Ctx(
+      id: UUID,
+      sp: Supervisor[IO],
+      log: Log
+  )
+
+  def withCtx(f: Ctx ?=> IO[Unit]): IO[Unit] =
+    val mkCtx = for
+      id <- Resource.eval(IO(UUID.randomUUID()))
+      sp <- Supervisor[IO]
+      lg <- Resource.eval(Ref.of[IO, List[String]](List.empty))
+    yield Ctx(id, sp, Log(lg))
+
+    mkCtx.use { ctx =>
+      f(using ctx) *> ctx.log.get.flatMap(_.traverse_(IO.println))
+    }
+
+  val p1: Ctx ?=> IO[Unit] =
+    val ctx = summon[Ctx]
+    IO.println("Running program #1")
+
+  def p2(using ctx: Ctx): IO[Unit] =
+    IO.println("Running program #2") *> p4 *>
+      ctx.sp
+        .supervise {
+          ctx.log.add(s"Start: ${ctx.id}") >>
+            IO.sleep(1.second) >>
+            ctx.log.add(s"Done: ${ctx.id}")
+        }
+        .flatMap { fb =>
+          ctx.log.add(s"Waiting: ${ctx.id}") >>
+            fb.join.void
+        }
+
+  val p3: Ctx ?=> IO[Unit] =
+    IO.sleep(100.millis) *> IO.println("Running program #3")
+
+  val p4: Ctx ?=> IO[Unit] =
+    IO.println("Running program #4")
+
+  val run: IO[Unit] =
+    withCtx {
+      p1 &> p2 &> p3
+    }
