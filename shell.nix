eae42a937a65e04d80a93303d3bd9b4c34f82e2d
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-04 14:57:27 +0100

    Use overlay to define JRE

--- shell.nix
+++ shell.nix
@@ -1,34 +1,27 @@
 { jdk ? "jdk17" }:
 
 let
-  config = {
-    packageOverrides = p: rec {
-      java = p.${jdk};
-
-      sbt = p.sbt.overrideAttrs (
-        old: rec {
-          patchPhase = ''
-            echo -java-home ${java} >> conf/sbtopts
-          '';
-        }
-      );
-    };
-  };
 
   nixpkgs = fetchTarball {
-    name   = "nixos-unstable-2021-10-27";
-    url    = "https://github.com/NixOS/nixpkgs/archive/a4bf44345706.tar.gz";
+    name = "nixos-unstable-2021-10-27";
+    url = "https://github.com/NixOS/nixpkgs/archive/a4bf44345706.tar.gz";
     sha256 = "0zag9yfqsf544vrfccfvn5yjagizqf69adza8fpmsmn5ll8jw8gw";
   };
 
-  pkgs = import nixpkgs { inherit config; };
+  pkgs = import nixpkgs {
+    overlays = [
+      (final: prev: {
+        jre = final.${jdk};
+      })
+    ];
+  };
 in
 pkgs.mkShell {
   name = "scala-shell";
 
   buildInputs = [
     pkgs.coursier
-    pkgs.${jdk}
+    pkgs.jre
     pkgs.sbt
   ];
 }
