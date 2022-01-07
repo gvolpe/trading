6d3e72004fb529e8b9f24213820c85357eb39007
Gabriel Volpe <volpegabriel@gmail.com>
2022-01-07 10:35:40 +0100

    switch to nix flakes

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Scala development shell";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      forSystem = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          jdk = pkgs.jdk17_headless;
+        in
+        {
+          devShell = pkgs.mkShell {
+            name = "scala-dev-shell";
+            buildInputs = [
+              jdk
+              pkgs.coursier
+              pkgs.sbt
+            ];
+            shellHook = ''
+              JAVA_HOME="${jdk}"
+            '';
+          };
+        };
+    in
+    flake-utils.lib.eachDefaultSystem forSystem;
+}
