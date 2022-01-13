8b405a4866a7242c2bb92e713e69107a9718f415
Gabriel Volpe <volpegabriel@gmail.com>
2022-01-13 14:45:53 +0100

    reject start command when trading status is on

--- modules/core/src/main/scala/trading/core/TradeEngine.scala
+++ modules/core/src/main/scala/trading/core/TradeEngine.scala
@@ -23,13 +23,15 @@ object TradeEngine:
         val nst = st.remove(symbol)(action, price)
         nst -> ((id, ts) => TradeEvent.CommandExecuted(id, cid, cmd, ts))
       // Trading switch: On / Off
-      case (st, TradeCommand.Start(_, cid, _)) =>
+      case (st @ TradeState(Off, _), TradeCommand.Start(_, cid, _)) =>
         val nst = TradeState._Status.replace(On)(st)
         nst -> ((id, ts) => TradeEvent.Started(id, cid, ts))
-      case (st, TradeCommand.Stop(_, cid, _)) =>
+      case (st @ TradeState(On, _), TradeCommand.Stop(_, cid, _)) =>
         val nst = TradeState._Status.replace(Off)(st)
         nst -> ((id, ts) => TradeEvent.Stopped(id, cid, ts))
       // Trading status: Off
       case (st @ TradeState(Off, _), cmd) =>
         st -> ((id, ts) => TradeEvent.CommandRejected(id, cmd.cid, cmd, Reason("Trading is Off"), ts))
+      case (st @ TradeState(On, _), cmd @ TradeCommand.Start(_, cid, _)) =>
+        st -> ((id, ts) => TradeEvent.CommandRejected(id, cid, cmd, Reason("Trading already On"), ts))
     }
