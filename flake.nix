e4a7532a6b446a00a18336f3f0e64ac81afe8971
Gabriel Volpe <volpegabriel@gmail.com>
2022-02-10 20:17:26 +0100

    use nixos-rebuild switch

--- flake.nix
+++ flake.nix
@@ -12,9 +12,30 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     let
+      kompose-overlay = f: p: {
+        kompose = p.buildGoModule rec {
+          pname = "kompose";
+          version = "1.26.1";
+
+          doCheck = false;
+
+          src = p.fetchFromGitHub {
+            rev = "v${version}";
+            owner = "kubernetes";
+            repo = "kompose";
+            sha256 = "sha256-NfzqGG5ZwPpmjhvcvXN1AA+kfZG/oujbAEtXkm1mzeU=";
+          };
+
+          vendorSha256 = "sha256-OR5U2PnebO0a+lwU09Dveh0Yxk91cmSRorTxQIO5lHc=";
+        };
+      };
+
       forSystem = system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ kompose-overlay ];
+          };
           jdk = pkgs.jdk17_headless;
         in
         {
@@ -23,6 +44,9 @@
             buildInputs = [
               jdk
               pkgs.coursier
+              pkgs.kubectl
+              pkgs.kompose
+              pkgs.minikube
               pkgs.sbt
             ];
             shellHook = ''
