541240d1bd3337b9efda378c62e54aa99ec50cee
Gabriel Volpe <volpegabriel@gmail.com>
2022-05-17 09:33:50 +0200

    update neutron to 0.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val monocle       = "3.1.0"
     val natchez       = "0.1.6"
     val natchezHttp4s = "0.3.2"
-    val neutron       = "0.4.0+15-6d8da965-SNAPSHOT"
+    val neutron       = "0.5.0"
     val odin          = "0.13.0"
     val redis4cats    = "1.1.1+37-bfb98081-SNAPSHOT"
     val refined       = "0.9.29"
