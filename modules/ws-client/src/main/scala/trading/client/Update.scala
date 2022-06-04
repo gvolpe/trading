f0518a5fd71cdcdc721e2b20b323f851835d2c70
Gabriel Volpe <volpegabriel@gmail.com>
2022-06-04 12:29:21 +0200

    update tyrian to 0.5.0 :partying_face:

--- modules/ws-client/src/main/scala/trading/client/Update.scala
+++ modules/ws-client/src/main/scala/trading/client/Update.scala
@@ -3,63 +3,65 @@ package trading.client
 import trading.domain.{ Alert, Symbol }
 import trading.ws.{ WsIn, WsOut }
 
+import cats.effect.IO
 import cats.syntax.eq.*
 import io.circe.syntax.*
 
 import org.scalajs.dom
 import tyrian.*
 import tyrian.cmds.Dom
-import tyrian.websocket.{ KeepAliveSettings, WebSocket }
+import tyrian.websocket.{ KeepAliveSettings, WebSocket, WebSocketConnect }
 
-def disconnected(model: Model): (Model, Cmd[Msg]) =
-  model.copy(error = Some("Disconnected from server, please click on Connect.")) -> Cmd.Empty
+def disconnected(model: Model): (Model, Cmd[IO, Msg]) =
+  model.copy(error = Some("Disconnected from server, please click on Connect.")) -> Cmd.None
 
-def refocusInput: Cmd[Msg] =
+def refocusInput: Cmd[IO, Msg] =
   Dom.focus("symbol-input")(_.fold(e => Msg.FocusError(e.elementId), _ => Msg.NoOp))
 
-def runUpdates(msg: Msg, model: Model): (Model, Cmd[Msg]) =
+def runUpdates(msg: Msg, model: Model): (Model, Cmd[IO, Msg]) =
   msg match
     case Msg.NoOp =>
-      model -> Cmd.Empty
+      model -> Cmd.None
 
     case Msg.FocusError(id) =>
-      model.copy(error = Some(s"Fail to focus on ID: $id")) -> Cmd.Empty
+      model.copy(error = Some(s"Fail to focus on ID: $id")) -> Cmd.None
 
     case Msg.ConnStatus(WsMsg.Connecting) =>
-      model -> WebSocket.connect(model.wsUrl, KeepAliveSettings.default)(
-        _.fold(WsMsg.Error(_), WsMsg.Connected(_)).asMsg
-      )
+      model -> WebSocket.connect[IO, Msg](model.wsUrl, KeepAliveSettings.default) {
+        case WebSocketConnect.Socket(s) => WsMsg.Connected(s).asMsg
+        case WebSocketConnect.Error(e)  => WsMsg.Error(e).asMsg
+      }
 
     case Msg.ConnStatus(WsMsg.Connected(ws)) =>
       model.copy(error = None, ws = Some(ws)) -> refocusInput
 
     case Msg.ConnStatus(WsMsg.Disconnected) =>
-      model.copy(socketId = None) -> Cmd.Empty
+      model.copy(socketId = None) -> Cmd.None
 
     case Msg.ConnStatus(WsMsg.Error(cause)) =>
-      model.copy(error = Some(s"Connection error: $cause")) -> Cmd.Empty
+      model.copy(error = Some(s"Connection error: $cause")) -> Cmd.None
 
     case Msg.ConnStatus(WsMsg.Heartbeat) =>
-      model -> model.ws.map(_.publish("{ \"Heartbeat\": {} }")).getOrElse(Cmd.Empty)
+      model -> model.ws.map(_.publish("{ \"Heartbeat\": {} }")).getOrElse(Cmd.None)
 
     case Msg.CloseAlerts =>
       model.copy(error = None, sub = None, unsub = None) -> refocusInput
 
     case Msg.SymbolChanged(in) if in.length == 6 =>
-      model.copy(symbol = Symbol.unsafeFrom(in), input = in) -> Cmd.Empty
+      model.copy(symbol = Symbol.unsafeFrom(in), input = in) -> Cmd.None
 
     case Msg.SymbolChanged(in) =>
-      model.copy(input = in) -> Cmd.Empty
+      model.copy(input = in) -> Cmd.None
 
     case Msg.Subscribe =>
-      if model.symbol === Symbol.XXXXXX then model.copy(error = Some("Invalid symbol")) -> Cmd.Empty
+      if model.symbol === Symbol.XXXXXX then model.copy(error = Some("Invalid symbol")) -> Cmd.None
       else
         model.socketId match
           case Some(_) =>
             val nm       = model.copy(sub = Some(model.symbol), symbol = Symbol.XXXXXX, input = "")
             val in: WsIn = WsIn.Subscribe(model.symbol)
             val cmd      = model.ws.map(ws => Cmd.Batch(ws.publish(in.asJson.noSpaces), refocusInput))
-            nm -> cmd.getOrElse(Cmd.Empty)
+            nm -> cmd.getOrElse(Cmd.None)
           case None =>
             disconnected(model)
 
@@ -69,15 +71,15 @@ def runUpdates(msg: Msg, model: Model): (Model, Cmd[Msg]) =
           val nm       = model.copy(unsub = Some(symbol), alerts = model.alerts - symbol)
           val in: WsIn = WsIn.Unsubscribe(symbol)
           val cmd      = model.ws.map(ws => Cmd.Batch(ws.publish(in.asJson.noSpaces), refocusInput))
-          nm -> cmd.getOrElse(Cmd.Empty)
+          nm -> cmd.getOrElse(Cmd.None)
         case None =>
           disconnected(model)
 
     case Msg.Recv(WsOut.Attached(sid, users)) =>
-      model.copy(socketId = Some(sid), onlineUsers = users.toInt) -> Cmd.Empty
+      model.copy(socketId = Some(sid), onlineUsers = users.toInt) -> Cmd.None
 
     case Msg.Recv(WsOut.Notification(t: Alert.TradeAlert)) =>
-      model.copy(alerts = model.alerts.updated(t.symbol, t)) -> Cmd.Empty
+      model.copy(alerts = model.alerts.updated(t.symbol, t)) -> Cmd.None
 
     case Msg.Recv(WsOut.Notification(t: Alert.TradeUpdate)) =>
-      model.copy(tradingStatus = t.status) -> Cmd.Empty
+      model.copy(tradingStatus = t.status) -> Cmd.None
--- modules/ws-client/src/main/scala/trading/client/WebApp.scala
+++ modules/ws-client/src/main/scala/trading/client/WebApp.scala
@@ -2,19 +2,21 @@ package trading.client
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
+import cats.effect.IO
+
 import tyrian.*
 
 @JSExportTopLevel("TyrianApp")
 object Main extends TyrianApp[Msg, Model]:
 
-  def init(flags: Map[String, String]): (Model, Cmd[Msg]) =
-    Model.init -> Cmd.Empty
+  def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    Model.init -> Cmd.None
 
-  def update(msg: Msg, model: Model): (Model, Cmd[Msg]) =
-    runUpdates(msg, model)
+  def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
+    runUpdates(_, model)
 
   def view(model: Model): Html[Msg] =
     render(model)
 
-  def subscriptions(model: Model): Sub[Msg] =
+  def subscriptions(model: Model): Sub[IO, Msg] =
     wsSub(model.ws)
