b1cb3ac81a221b50b16f5d9fc7d194da7e276f77
Gabriel Volpe <volpegabriel@gmail.com>
2022-07-24 18:57:04 +0200

    invoke g function only once

--- modules/domain/shared/src/main/scala/trading/state/TradeState.scala
+++ modules/domain/shared/src/main/scala/trading/state/TradeState.scala
@@ -2,18 +2,21 @@ package trading.state
 
 import trading.domain.*
 
+import cats.{ Eq, Show }
 import cats.derived.*
 import cats.syntax.all.*
-import cats.{ Eq, Show }
 import io.circe.*
 import monocle.function.{ At, Index }
 import monocle.{ Focus, Optional }
 
+// format: off
 final case class TradeState(
     status: TradingStatus,
     prices: TradeState.SymbolPrices
 ) derives Eq, Show:
-  def modify(symbol: Symbol)(action: TradeAction, price: Price, quantity: Quantity): TradeState = {
+// format: on
+
+  def modify(symbol: Symbol)(action: TradeAction, price: Price, quantity: Quantity): TradeState =
     val h = Prices._High.modify(p => if price > p then price else p)(_)
     val l = Prices._Low.modify(p => if price < p || p === Price(0.0) then price else p)(_)
 
@@ -21,12 +24,11 @@ final case class TradeState(
       case TradeAction.Ask =>
         val f = Prices._Ask.modify(_.updated(price, quantity))(_)
         val g = f.andThen(h).andThen(l)
-        TradeState.__Prices.at(symbol).modify(_.fold(g(Prices.empty))(g).some)(this)
+        TradeState.__Prices.at(symbol).modify(_.orElse(Prices.empty.some).map(g))(this)
       case TradeAction.Bid =>
         val f = Prices._Bid.modify(_.updated(price, quantity))(_)
         val g = f.andThen(h).andThen(l)
-        TradeState.__Prices.at(symbol).modify(_.fold(g(Prices.empty))(g).some)(this)
-  }
+        TradeState.__Prices.at(symbol).modify(_.orElse(Prices.empty.some).map(g))(this)
 
   def remove(symbol: Symbol)(action: TradeAction, price: Price): TradeState =
     action match
@@ -62,12 +64,14 @@ object TradeState:
     def apply(s: Symbol): Optional[TradeState, Prices.Bid] =
       __Prices.index(s).andThen(Prices._Bid)
 
+// format: off
 final case class Prices(
     ask: Prices.Ask,
     bid: Prices.Bid,
     high: Price,
     low: Price
 ) derives Eq, Show
+// format: on
 
 object Prices:
   def empty: Prices = Prices(Map.empty, Map.empty, Price(0.0), Price(0.0))
