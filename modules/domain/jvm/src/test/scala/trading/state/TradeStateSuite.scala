27c7435d3ca550fcd2b67d78326679639ccbf036
Gabriel Volpe <volpegabriel@gmail.com>
2022-09-30 14:53:58 +0200

    reduce min successful tests to speed up test suite

--- modules/domain/jvm/src/test/scala/trading/state/TradeStateSuite.scala
+++ modules/domain/jvm/src/test/scala/trading/state/TradeStateSuite.scala
@@ -6,6 +6,7 @@ import trading.domain.cogen.given
 
 import monocle.law.discipline.*
 import org.scalacheck.Arbitrary
+import org.scalacheck.Test.Parameters
 import weaver.FunSuite
 import weaver.discipline.Discipline
 
@@ -17,9 +18,11 @@ object TradeStateSuite extends FunSuite with Discipline:
   val askPrice: AskPrice = Price(1.6537)
   val bidPrice: BidPrice = Price(1.3908)
 
-  checkAll("__AtAsk Optional", OptionalTests(__AtAsk(askPrice)))
-  checkAll("__AtBid Optional", OptionalTests(__AtBid(bidPrice)))
-  checkAll("__Prices At Optional", OptionalTests(__Prices.at(symbol)))
-  checkAll("__Prices Index Optional", OptionalTests(__Prices.index(symbol)))
-  checkAll("__AskPrices Optional", OptionalTests(__AskPrices(symbol)))
-  checkAll("__BidPrices Optional", OptionalTests(__BidPrices(symbol)))
+  val p: Parameters => Parameters = _.withMinSuccessfulTests(10)
+
+  checkAll("__AtAsk Optional", OptionalTests(__AtAsk(askPrice)), p)
+  checkAll("__AtBid Optional", OptionalTests(__AtBid(bidPrice)), p)
+  checkAll("__Prices At Optional", OptionalTests(__Prices.at(symbol)), p)
+  checkAll("__Prices Index Optional", OptionalTests(__Prices.index(symbol)), p)
+  checkAll("__AskPrices Optional", OptionalTests(__AskPrices(symbol)), p)
+  checkAll("__BidPrices Optional", OptionalTests(__BidPrices(symbol)), p)
