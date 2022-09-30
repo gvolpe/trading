61bb21062402fc3feea8b8b90e739ec217050d1a
Gabriel Volpe <volpegabriel@gmail.com>
2022-09-30 14:38:09 +0200

    cats-effect: update to 3.4.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   object V {
     val cats          = "2.8.0"
-    val catsEffect    = "3.3.14"
+    val catsEffect    = "3.4.0-RC1"
     val circe         = "0.14.3"
     val ciris         = "2.4.0"
     val doobie        = "1.0.0-RC2"
