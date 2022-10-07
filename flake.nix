dd54b7e76f4e1068f8ea76c10950728faa45b2d3
Gabriel Volpe <volpegabriel@gmail.com>
2022-10-07 11:26:14 +0200

    nix: replace flake-compat with builtins.getFlake

--- flake.nix
+++ flake.nix
@@ -4,72 +4,66 @@
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
-    flake-compat = {
-      url = github:edolstra/flake-compat;
-      flake = false;
-    };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      forSystem = system:
-        let
-          dockerOverlay = f: p: with f.dockerTools; {
-            # September 2022: this docker image stopped working with sbt-native-packager
-            noRootDockerImage = buildLayeredImage {
-              name = "jdk17-curl";
-              tag = "latest";
-              contents = [ f.curl f.jre ];
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        dockerOverlay = f: p: with f.dockerTools; {
+          # September 2022: this docker image stopped working with sbt-native-packager
+          noRootDockerImage = buildLayeredImage {
+            name = "jdk17-curl";
+            tag = "latest";
+            contents = [ f.curl f.jre ];
+          };
+
+          slimDockerImage = buildImage {
+            name = "jdk17-curl";
+            fromImage = pullImage {
+              imageName = "openjdk";
+              imageDigest = "sha256:2e7658fb62d1c6f319ff9870614deaf8e06038dd41eec3d1ecdcfabd186234fd";
+              sha256 = "0m47vk8k97xqgc6bnxhmyyyvqnrn5jdqx8rw417740963h4i813v";
+              finalImageName = "openjdk";
+              finalImageTag = "17-jdk-slim-buster";
+              os = "linux";
+              arch = "amd64";
             };
-
-            slimDockerImage = buildImage {
-              name = "jdk17-curl";
-              fromImage = pullImage {
-                imageName = "openjdk";
-                imageDigest = "sha256:2e7658fb62d1c6f319ff9870614deaf8e06038dd41eec3d1ecdcfabd186234fd";
-                sha256 = "0m47vk8k97xqgc6bnxhmyyyvqnrn5jdqx8rw417740963h4i813v";
-                finalImageName = "openjdk";
-                finalImageTag = "17-jdk-slim-buster";
-                os = "linux";
-                arch = "amd64";
-              };
-              tag = "latest";
-              copyToRoot = [ p.curl ];
-            };
+            tag = "latest";
+            copyToRoot = [ p.curl ];
           };
-
-          jreOverlay = f: p: {
-            jre = p.jdk17_headless;
-          };
-
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ dockerOverlay jreOverlay ];
-          };
-        in
-        {
-          devShell = pkgs.mkShell {
-            name = "scala-dev-shell";
-            buildInputs = with pkgs; [
-              coursier
-              envsubst
-              jre
-              kubectl
-              minikube
-              sbt
-            ];
-            shellHook = ''
-              JAVA_HOME="${pkgs.jre}"
-            '';
-          };
-
-          packages = {
-            docker = pkgs.noRootDockerImage;
-            slimDocker = pkgs.slimDockerImage;
-          };
-
-          defaultPackage = pkgs.slimDockerImage;
         };
-    in
-    flake-utils.lib.eachDefaultSystem forSystem;
+
+        jreOverlay = f: p: {
+          jre = p.jdk17_headless;
+        };
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ dockerOverlay jreOverlay ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "scala-dev-shell";
+          buildInputs = with pkgs; [
+            coursier
+            envsubst
+            jre
+            kubectl
+            minikube
+            sbt
+          ];
+          shellHook = ''
+            JAVA_HOME="${pkgs.jre}"
+          '';
+        };
+
+        packages = {
+          docker = pkgs.noRootDockerImage;
+          slimDocker = pkgs.slimDockerImage;
+        };
+
+        defaultPackage = pkgs.slimDockerImage;
+      }
+    );
 }
--- shell.nix
+++ shell.nix
@@ -1,10 +1,3 @@
-(import
-  (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  { src = ./.; }
-).shellNix
+{ system ? builtins.currentSystem or "unknown-system" }:
+
+(builtins.getFlake ("git+file://" + toString ./.)).devShell.${system}
