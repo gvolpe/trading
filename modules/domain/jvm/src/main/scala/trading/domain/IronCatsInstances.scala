49edcd0575d4b7da89a7b9160e3037863032257f
Gabriel Volpe <volpegabriel@gmail.com>
2022-12-19 18:38:01 +0100

    update iron to latest nightly (#208)

--- modules/domain/jvm/src/main/scala/trading/domain/IronCatsInstances.scala
+++
@@ -1,9 +0,0 @@
-package trading.domain
-
-import cats.*
-import io.github.iltotore.iron.*
-
-object IronCatsInstances:
-  inline given [A, B](using inline ev: Eq[A]): Eq[A :| B] = ev.asInstanceOf[Eq[A :| B]]
-  inline given [A, B](using inline ev: Order[A]): Order[A :| B] = ev.asInstanceOf[Order[A :| B]]
-  inline given [A, B](using inline ev: Show[A]): Show[A :| B] = ev.asInstanceOf[Show[A :| B]]
