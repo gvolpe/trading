5dcf259dcb4d6b588f67d8f108beedd6b4af6242
Gabriel Volpe <volpegabriel@gmail.com>
2024-04-22 22:28:37 +0200

    update scala to 3.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     val weaver     = "0.8.3"
 
     val organizeImports = "0.6.0"
-    val zerowaste       = "0.2.18"
+    val zerowaste       = "0.2.19"
   }
 
   object Libraries {
